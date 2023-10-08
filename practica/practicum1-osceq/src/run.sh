4d33c0231074c2dec2bf436ee43e36817d908f31
Daan Degrauwe <daan.degrauwe@meteo.be>
2023-10-08 16:27:37 +0200

    Created notebook for practicum on python basics

--- practica/practicum1-osceq/src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm osceq output.dat
-
-gfortran constants.F90 setup_constants.F90 timeloop.F90 write_result.F90 osceq.F90 -o osceq
-
-./osceq
-
--- practica/practicum2-adveq/solutions/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for exercise in upstream forward_spectral leapfrog_centered trapezium_spectral forward_centered heun_centered matsuno_centered; do
-	cd ${exercise}
-	./run.sh
-	cd ..
-done
